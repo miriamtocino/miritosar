2d4915e6f4ac7c36862b74e962717d91e35245cd
Miriam Tocino <miriam.tocino@gmail.com>
2015-04-08 18:09:08 +0200

    Basic styles ready to keep working on the backend

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -1,2 +1,16 @@
+require 'uri'
+
 module CompaniesHelper
+  def image_for(company)
+    if company.logo.blank?
+      image_tag('placeholder.png')
+    else
+      image_tag(company.logo)
+    end
+  end
+
+  def website_for(website)
+    clean_url = URI.split(website)
+    clean_url[2]
+  end
 end
