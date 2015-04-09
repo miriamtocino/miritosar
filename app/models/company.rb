4e43bb0204a2fff87ec4f90a9544bf36cb204e51
Miriam Tocino <miriam.tocino@gmail.com>
2015-04-09 11:02:06 +0200

    Changing small, medium and large companies to be found by scopes

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,15 +1,7 @@
 class Company < ActiveRecord::Base
   default_scope { order('name ASC') }
 
-  def small?
-    staff < 15
-  end
-
-  def medium?
-    staff >= 15 || staff <= 50
-  end
-
-  def large?
-    staff > 50
-  end
+  scope :small, -> { where('staff < 15') }
+  scope :medium, -> { where('staff >= 15 || staff <= 50') }
+  scope :large, -> { where('staff > 50') }
 end
