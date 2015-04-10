36b2e42cb6da7ee9f1577d187244fddcf252ee91
Miriam Tocino <miriam.tocino@gmail.com>
2015-04-10 14:42:33 +0200

    Review form added to company show page using AJAX

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,59 +1,19 @@
 class ReviewsController < ApplicationController
-  before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_review, only: [:destroy]
 
-  # GET /reviews
-  # GET /reviews.json
-  def index
-    @company = Company.find(params[:company_id])
-    @reviews = @company.reviews
-  end
-
-  # GET /reviews/1
-  # GET /reviews/1.json
-  def show
-  end
-
-  # GET /reviews/new
-  def new
-    @review = Review.new
-  end
-
-  # GET /reviews/1/edit
-  def edit
-  end
-
-  # POST /reviews
-  # POST /reviews.json
+  # POST companies/1/reviews
   def create
-    @review = Review.new(review_params)
+    @company = Company.find(params[:company_id])
+    @review = @company.reviews.new(review_params)
+    @review.save!
 
     respond_to do |format|
-      if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
-        format.json { render :show, status: :created, location: @review }
-      else
-        format.html { render :new }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-      end
+      format.html { redirect_to @company }
+      format.js # render reviews/create.js.erb
     end
   end
 
-  # PATCH/PUT /reviews/1
-  # PATCH/PUT /reviews/1.json
-  def update
-    respond_to do |format|
-      if @review.update(review_params)
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
-        format.json { render :show, status: :ok, location: @review }
-      else
-        format.html { render :edit }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /reviews/1
-  # DELETE /reviews/1.json
+  # DELETE companies/1/reviews/1
   def destroy
     @review.destroy
     respond_to do |format|
@@ -65,7 +25,7 @@ class ReviewsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_review
-      @review = Review.find(params[:id])
+      @review = Review.find(params[:review_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
