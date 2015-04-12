4da7277f3e4452f8c9d465f48dd5eb5213b374d0
Miriam Tocino <miriam.tocino@gmail.com>
2015-04-12 18:09:16 +0200

    Starting to customise Devise views | Including Sign In and Registration links in header | To-do : How to add name to devise model?

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :name, presence: true
-  validates :email, presence: true,
-                    format: /\A\S+@\S+\z/,
-                    uniqueness: { case_sensitive: false }
+  # validates :name, presence: true
+  # validates :email, presence: true,
+                    # format: /\A\S+@\S+\z/,
+                    # uniqueness: { case_sensitive: false }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   resources :companies do
     resources :reviews
   end
+
+  resources :users
 end
